71f0f049ae43d1c37c03b560429946a16f538857
vitoria.araujo <vitoria.araujo@ceuma.com>
2024-04-01 17:14:58 -0300

    feat: criando compoente Participant

---
+++ src/components/Participant/index.tsx
@@ -0,0 +1,15 @@
+import { Text, TouchableOpacity, View, ViewBase } from "react-native";
+import { styles } from "./styles";
+type Props = {
+    name: string
+}
+export function Participant({name}:Props){
+    return(
+        <View style={styles.container}>
+            <Text style={styles.name}>{name}</Text>
+              <TouchableOpacity style={styles.button}>
+                    <Text style={styles.buttonText}>-</Text>
+                </TouchableOpacity>
+        </View>
+    )
+}
---
+++ src/components/Participant/styles.ts
@@ -0,0 +1,30 @@
+import { StyleSheet } from "react-native";
+
+export const styles = StyleSheet.create({
+    container: {
+        width:'100%',
+        backgroundColor: '#1f1e25',
+        flexDirection: 'row',
+        alignItems:'center',
+        justifyContent: 'space-between',
+        borderRadius:5,
+        marginBottom:10,
+    },
+    name:{
+        color:'#fff',
+        fontSize:20,
+        marginLeft: 16,
+    },
+    buttonText:{
+    color:'#fff',
+    fontSize:24
+},
+button: {
+    width:56,
+    height:56,
+    borderRadius:5,
+    backgroundColor:'#e23c44',
+    alignItems:'center',
+    justifyContent:'center'
+},
+})
